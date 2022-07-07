1f13b79987649dde6f3e1b292fc61fbc61ed3c49
Marthe.Aastveit <aastveit@nr.no>
2022-07-07 16:21:37 +0200

    Divided smms into hessian and optim

--- R/sm_msm_optim_func.R
+++ R/sm_msm_optim_func.R
@@ -1,37 +1,4 @@
-library(numDeriv)
-## NOT DONE
-# start_params = function(data_set, graph){
-#   names_surv_dens = names_of_survial_density(graph)
-#   which_abs = which(names_surv_dens[,"type"] == "abs")
-#   edge_abs = names_surv_dens[which_abs, "all_transitions"]
-#   all_data_set = arrange_data(data_set, graph)
-#   midpoint = matrix(NA, nrow = nrow(all_data_set), ncol = 6)
-#   for(i in 1:nrow(all_data_set)){
-#     observation_type[i] = all_data_set[i,"obs_type"]
-#     type = names(which(formula_obs_types[, observation_type[i]] == 1))
-#     time_points = all_data_set[i,1:(ncol(all_data_set)-1)]
-#     if( observational_type[i] == type){
-#       integral_limits = finding_limits_integral(i, type, graph, all_edges, absorbing_state_new, all_data_set)
-#       na_omit_intergal_limits = integral_limits[!is.na(integral_limits)]
-#       if("tmin_int1" %in% names(na_omit_intergal_limits)){
-#         midpoint[i, 1] = (na_omit_intergal_limits["tmax_int1"]-na_omit_intergal_limits["tmin_int1"])/2
-#       }
-#       if("tmin_int2" %in% names(na_omit_intergal_limits)){
-#         midpoint[i, 2] = midpoint[i, 1] + (na_omit_intergal_limits["tmax_int2"]-na_omit_intergal_limits["tmin_int2"])/2
-#       }
-#       if("tmin_int3" %in% names(na_omit_intergal_limits)){
-#         midpoint[i, 3] = midpoint[i, 2] + (na_omit_intergal_limits["tmax_int3"]-na_omit_intergal_limits["tmin_int3"])/2
-#       }
-#       if("tmin_int4" %in% names(na_omit_intergal_limits)){
-#         midpoint[i, 4] = midpoint[i, 3] + (na_omit_intergal_limits["tmax_int4"]-na_omit_intergal_limits["tmin_int4"])/2
-#       }
-#       midpoint[i, 5] = na_omit_intergal_limits["tmax"]
-#     } else{
-#       time_points = all_data_set[i,1:(ncol(all_data_set)-1)]
-#     }
-#   }
-# }
-
+### Multi-state functions part 3 - general smms function, and functions for quantities of interest
 #' Fit a semi-Markovian multistate model
 #'
 #' The function the user should interact with. Assumes that the user has provided densities and 
@@ -55,7 +22,7 @@ library(numDeriv)
 # This function needs to be updated so that it takes column names as input: to know which column corresponds
 # to "patient" (numbering or names for each patient), "time" (the time when a patient was observed),
 # "state" (the state which the patient occupies at the observation time).
-smms = function(startval, data, graph, X = NULL, mc_cores = 3){
+smms = function(startval, data, graph, X = NULL, mc_cores = 3, variance_matrix = FALSE){
   formula_obs_types = all_types(graph)
   edge_mats = edge_matrices(graph)
   state_ord = state_ordering(graph)
@@ -81,18 +48,65 @@ smms = function(startval, data, graph, X = NULL, mc_cores = 3){
     all_integral_limits[[i]] = integral_mellomregn
     integrand[[i]] = integrand_mellomregn
   }
-
-    
+  
+  
   optimizer <- optim(startval,mloglikelihood,integrand = integrand,limits = all_integral_limits,X=X, method = "L-BFGS",
-               mc_cores=mc_cores,hessian = FALSE)
-  hessian_optimizer = numDeriv::hessian(mloglikelihood, optimizer$par, integrand = integrand,limits = all_integral_limits,
-                              mc_cores=mc_cores,X=X)
-  return(list(optimizer, hessian_optimizer))
+                     mc_cores=mc_cores,hessian = FALSE)
+  if(variance_matrix == TRUE){
+    hessian_optimizer = numDeriv::hessian(mloglikelihood, optimizer$par, integrand = integrand,limits = all_integral_limits,
+                                          mc_cores=mc_cores,X=X)
+    return(list(optimizer, hessian_optimizer))
+  } else{
+    return(optimizer)
+  }
 }
 
-#optim_func(params = params, dd, gg)
-
-
+#' Estimates the hessian function confidence intervals
+#'
+#' Provide estimates and approximate confidence intervals for all parameters 
+#' (on the "original" scale - meaning that all parameters live on -Inf to +Inf).
+#'
+#' @param param  The parameter estimates from optimizer$par in smms.
+#' @param data A data frame where the number of rows correspond to the total number of observations for all patients,
+#' and  with 3 columns: patient (numbering or names for each patient), time (the time when a patient was observed),
+#' state (the state which the patient occupies at the observation time). FOR NOW!!
+#' @param graph A directed, acyclic graph giving the multistate structure, in the igraph format (igraph package).
+#' @param X A matrix with the covariates. Number of rows equal to the number of patients and one column
+#' for each covariate. The covariate specification is given by the user-specified densities.
+#' @param mc_cores The number of cores to use (for parallelisation). The function uses the mclapply()
+#' function from the parallel package.
+#' @return A vector of the same length as time.
+#' 
+variance_matrix = function(param, data, graph, X = NULL, mc_cores = 3){
+  formula_obs_types = all_types(graph)
+  edge_mats = edge_matrices(graph)
+  state_ord = state_ordering(graph)
+  names_surv_dens = names_of_survival_density(graph)
+  absorbing_states <- sort(state_ord$order[which(state_ord$type=="abs")])
+  
+  all_data_set = arrange_data(data, graph)
+  timepointMat <- all_data_set[,1:(dim(all_data_set)[2]-1)]
+  
+  observation_type = rep(NA, nrow(all_data_set))
+  all_integral_limits = list()
+  integrand = list()
+  
+  for(i in 1:nrow(all_data_set)){
+    observation_type[i] = all_data_set[i,"obs_type"]
+    f_types = names(which(formula_obs_types[, observation_type[i]] == 1))
+    integrand_mellomregn = list()
+    integral_mellomregn= list()
+    for(j in 1:length(f_types)){
+      integrand_mellomregn[[j]] = eval(parse(text=type_to_integrand(f_types[j], edge_mats, names_surv_dens)))
+      integral_mellomregn[[j]] = finding_limits(timepointMat[i,],f_types[j],edge_mats,absorbing_states)
+    }
+    all_integral_limits[[i]] = integral_mellomregn
+    integrand[[i]] = integrand_mellomregn
+  }
+  hessian_optimizer = numDeriv::hessian(mloglikelihood, param, integrand = integrand,limits = all_integral_limits,
+                                        mc_cores=mc_cores,X=X)
+  return(hessian_optimizer)
+}
 
 #' Estimates and confidence intervals
 #'
@@ -391,4 +405,8 @@ overall_survival_delta = function(time, param, graph, xval = NULL){
   return(op)
 }
 
-
+###
+cubint <- function(integrand,lower,upper,tmax,param,xval){
+  cubintegrate(integrand, lower = lower,upper = upper, method = "divonne", maxEval = 500,
+               tt = tmax[1], tt2=tmax[2],param = param, x = xval)$integral
+}
